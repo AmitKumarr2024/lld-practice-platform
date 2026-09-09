e175bd6edc5238b67305647249edb545613be87a
Amit kumar <codes.amitkumar@gmail.com>
2026-09-09 23:35:03 +0530

    fix the cors issue 10

--- backend/src/app.js
+++ backend/src/app.js
@@ -19,6 +19,7 @@ function createApp() {
         const allowedOrigins = [
           "http://localhost:5173",
           "https://lld-practice-platform-alpha.vercel.app",
+          "https://lld-practice-platform-y1rz.vercel.app",
           "https://lld-practice-platform-y1rz.vercel.app"
         ];
 
