53f3491102e0e25f4f25050ce1538e175c908fc4
Amit kumar <codes.amitkumar@gmail.com>
2026-09-09 23:37:46 +0530

    fix the cors issue 11

--- backend/src/app.js
+++ backend/src/app.js
@@ -19,8 +19,6 @@ function createApp() {
         const allowedOrigins = [
           "http://localhost:5173",
           "https://lld-practice-platform-alpha.vercel.app",
-          "https://lld-practice-platform-y1rz.vercel.app",
-          "https://lld-practice-platform-y1rz.vercel.app"
         ];
 
         // Allow requests without an Origin header
